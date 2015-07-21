c587a7762de8b6085d36188d5d6d569ab9e7e456
q2939606 <mac@yangyue.local>
2015-07-21 10:21:56 +0800

    初始化项目

---
+++ yRead/yRead/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yRead
+//
+//  Created by mac on 15/7/21.
+//  Copyright (c) 2015年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yRead/yRead/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yRead
+//
+//  Created by mac on 15/7/21.
+//  Copyright (c) 2015年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
