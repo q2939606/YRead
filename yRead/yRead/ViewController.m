46ad27ec5e3a3c38f58490433a2c50acb6649a15
q2939606 <mac@yangyue.local>
2015-07-21 10:26:39 +0800

    呵呵了

--- yRead/yRead/ViewController.m
+++ yRead/yRead/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+     NSLog(@"尝试下高科技");
 }
 
 - (void)didReceiveMemoryWarning {
